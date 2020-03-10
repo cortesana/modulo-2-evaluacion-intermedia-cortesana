f2b2a15abddcb852f22a2c12a51e1248978efdc7
A CH <ach@MacBook-Pro-de-A.local>
2020-03-10 18:15:01 +0100

    Final Commit Everything working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 'use strict'
+
+const chosenNum = document.querySelector('#chosenNum');
+const clue = document.querySelector('#clue');
+const attempts = document.querySelector('#attempts');
+const tryNum = document.querySelector('#try-number');
+let randNum;
+
+function generateRandomNumber() {
+    randNum = Math.ceil(Math.random() * 100);
+    console.log(`Mi número aleatorio es ${randNum}`);
+}
+
+function increaseAttemptCounter() {
+    let attemptCounter = parseInt(attempts.innerHTML);
+    attemptCounter++;
+    attempts.innerHTML = attemptCounter;
+}
+
+function compareGuess() {
+    if(chosenNum.value < randNum) {
+        clue.innerHTML = 'Demasiado bajo.'
+    }
+    else if(chosenNum.value > randNum) {
+        clue.innerHTML = 'Demasiado alto.'
+    }
+    else {
+        clue.innerHTML = 'Has ganado, máquina'
+    }
+}
+
+function guessTheNumber() {
+    if(!(chosenNum.value < 101 && chosenNum.value > 0)) {
+        clue.innerHTML = 'El número debe estar entre 1 y 100.';
+    }
+    else {
+        increaseAttemptCounter();
+        compareGuess();
+    }
+}
+
+generateRandomNumber();
+tryNum.addEventListener('click', guessTheNumber);
